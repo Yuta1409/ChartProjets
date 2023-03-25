c66a4e4de4bb3ad0ba998eabbd10f2a30c44de2c
Yuta1409 <roger.yan@educ-valadon-limoges.fr>
2023-03-25 15:46:43 +0100

    commit

---
+++ assets/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Site</title>
+</head>
+<body>
+<h1>My Site</h1>
+<p>Welcome to my site!</p>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
